3b30e848f139545d986ada4acf5c7d1b6af7d941
aksel avetisyan <hello.dotoday@gmail.com>
2020-05-05 16:32:48 +0400

    homework 6

--- homework6.js
+++ homework6.js
@@ -1,1 +1,36 @@
-asd
+// 1
+function areUniqueElements(arr) {
+    let uniqueEls = new Set()
+    for (let el of arr) {
+        uniqueEls.add(el)
+    }
+    return arr.length === uniqueEls.size
+}
+
+// 2
+function sumOfEvenQuadratic(arr) {
+    return arr.filter(el => el % 2 === 0).reduce((cur, el) => Math.pow(el, 2) + cur, Math.pow(0, 2))
+}
+
+// 3
+function isPalindrome(str) {
+    let left = str.substring(0, str.length / 2)
+    let right = ''
+    if (str.length % 2 === 0) {
+        right = str.substring(str.length / 2, str.length).split('').reverse().join('')
+    } else {
+        right = str.substring((str.length / 2) + 1, str.length).split('').reverse().join('')
+    }
+    return left === right
+}
+
+// 4
+function getAnagramWords(word, arr) {
+    let anagramWords = []
+    arr.forEach(el => {
+        if (word.split('').sort().join('') === el.split('').sort().join('')) {
+            anagramWords.push(el)
+        }
+    })
+    return anagramWords
+}
